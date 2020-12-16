7f83043f88e7378b1c24c17d527f83ae26551213
Lilibeth Santo <itzlilibeth@gmail.com>
2020-12-16 03:24:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(array)
   array.collect{|name| "Hello, my name is #{name}."}
 end
+
 #
 # def assign_rooms
 #
