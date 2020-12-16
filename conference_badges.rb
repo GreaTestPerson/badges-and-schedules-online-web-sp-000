27e621c64ffcbd0c34a8b23c3d139307f36376c2
Lilibeth Santo <itzlilibeth@gmail.com>
2020-12-16 03:28:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,11 +7,14 @@ def batch_badge_creator(array)
   array.collect{|name| "Hello, my name is #{name}."}
 end
 
-#
-# def assign_rooms
-#
-# end
-#
-# def printer
-#
-# end
+def assign_rooms(array)
+  updated = []
+     array.each_with_index do |name, index| updated << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+     end
+ return updated
+end
+
+def printer
+batch_badge_creator(array).each do |index| puts "#{index}" end
+    assign_rooms(array).each do |index| puts "#{index}" end
+end
