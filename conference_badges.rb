9388e34088fc86220b64ef5bab028a25676dee2e
Lilibeth Santo <itzlilibeth@gmail.com>
2020-12-16 03:17:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,16 @@
 # Write your code here.
-def bagde_maker
-  name = []
- put "Hello, my name is #{}"
+def bagde_maker(name)
+ put "Hello, my name is #{name}"
 end
+
+# def batch_badge_creator
+#
+# end
+#
+# def assign_rooms
+#
+# end
+#
+# def printer
+#
+# end
